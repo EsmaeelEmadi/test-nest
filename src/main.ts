418ea02d0a3839a8c6b1b226e5c1447d009c9463
emad <emadi68moh@gmail.com>
2024-02-15 21:23:29 +0000

    idk

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,23 @@ import { AllExceptionsFilter } from './utils/exceptions';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new AllExceptionsFilter());
+  await app.listen(3000);
+
+
+  const microservice = app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.RMQ,
     options: {
-      urls: ['amqp://user:password@localhost:5672'], // Your RabbitMQ server URL
-      queue: 'emailQueue', // Make sure this matches the queue used in UserService
+      urls: ['amqp://user:password@localhost:5672'],
+      queue: 'emailQueue',
       queueOptions: {
         durable: false,
       },
     },
   });
 
-  app.useGlobalFilters(new AllExceptionsFilter());
-  await app.listen();
+  await app.startAllMicroservices();
 }
 bootstrap();
 
